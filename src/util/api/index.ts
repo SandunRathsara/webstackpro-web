2036b2457ec110e7bfbe8cab368a96083aa5a8f9
Sandun Rathsara <sandun@surge.global>
2023-12-19 14:22:48 +0530

    Backend configured to use Keycloak authentication

--- src/util/api/index.ts
+++ src/util/api/index.ts
@@ -1,8 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { TODO_TAG_CONFIG } from "@/features/todo/api/todo.tags";
+import { getSession } from "next-auth/react";
 
 const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    prepareHeaders: async (headers) => {
+      const session = await getSession();
+      headers.set("Authorization", `Bearer ${session?.token.accessToken}`);
+      return headers;
+    },
+  }),
   tagTypes: [...TODO_TAG_CONFIG],
   endpoints: () => ({}),
   reducerPath: "webstackpro-reducer",
